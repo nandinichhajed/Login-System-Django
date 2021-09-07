01874d99d497de415b260ecc3471fca292cc5f7d
nandinichhajed <nandinichhajed08@gmail.com>
2021-09-07 16:40:58 +0530

    Updating Authentication module

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
-    path('signout', views.signout, name='signout'),    
+    path('signout', views.signout, name='signout'),
+    path('activate/<uidb64>/<token>', views.activate, name='activate')    
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,15 +1,145 @@
-from django.shortcuts import render
-from django.http import HttpResponse 
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.core.mail import EmailMessage, send_mail
+from login import settings
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.utils.encoding import force_bytes, force_text
+from django.contrib.auth import authenticate, login, logout
+from Tools.scripts import generate_token
+from . tokens import generate_token
 
 # Create your views here.
 def home(request):
-    return HttpResponse("hello I am World")
+    return render(request, "authentication/index.html")
 
 def signup(request):
-    return render("authentication/signup.html")
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+        
+        if User.objects.filter(username=username):
+            messages.error(request, "Username already exist! Please try some other username.")
+            return redirect('home')
+        
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "Email Already Registered!!")
+            return redirect('home')
+        
+        if len(username)>20:
+            messages.error(request, "Username must be under 20 charcters!!")
+            return redirect('home')
+        
+        if pass1 != pass2:
+            messages.error(request, "Passwords didn't matched!!")
+            return redirect('home')
+        
+        if not username.isalnum():
+            messages.error(request, "Username must be Alpha-Numeric!!")
+            return redirect('home')
+        
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        # myuser.is_active = False
+        myuser.is_active = False
+        myuser.save()
+        messages.success(request, "Your Account has been created succesfully!! Please check your email to confirm your email address in order to activate your account.")
+        
+        # Welcome Email
+        subject = "Welcome to Our Website!!"
+        message = "Hello " + myuser.first_name + "!! \n" + "Welcome to Our Website!! \nThank-you for visiting our website.\n We have also sent you a conformation email, please conform your email address. \n\nThanking You\nNandin Chhajed"        
+        from_email = settings.EMAIL_HOST_USER
+        to_list = [myuser.email]
+        send_mail(subject, message, from_email, to_list, fail_silently=True)
+        
+        # Email Address Confirmation Email
+        current_site = get_current_site(request)
+        email_subject = "Confirm your Email Address"
+        message2 = render_to_string('email_confirmation.html',{
+            
+            'name': myuser.first_name,
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
+            'token': generate_token.make_token(myuser)
+        })
+        email = EmailMessage(
+        email_subject,
+        message2,
+        settings.EMAIL_HOST_USER,
+        [myuser.email],
+        )
+        email.fail_silently = True
+        email.send()
+        
+        return redirect('signin')
+        
+        
+    return render(request, "authentication/signup.html")
+
+
+def activate(request,uidb64,token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        myuser = User.objects.get(pk=uid)
+    except (TypeError,ValueError,OverflowError,User.DoesNotExist):
+        myuser = None
+
+    if myuser is not None and generate_token.check_token(myuser,token):
+        myuser.is_active = True
+        # user.profile.signup_confirmation = True
+        myuser.save()
+        login(request,myuser)
+        messages.success(request, "Your Account has been activated!!")
+        return redirect('signin')
+    else:
+        return render(request,'activation_failed.html')
+
 
 def signin(request):
-    return render("authentication/signin.html")
+    if request.method == 'POST':
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+        
+        user = authenticate(username=username, password=pass1)
+        
+        if user is not None:
+            login(request, user)
+            fname = user.first_name
+            # messages.success(request, "Logged In Sucessfully!!")
+            return render(request, "authentication/index.html",{"fname":fname})
+        else:
+            messages.error(request, "Bad Credentials!!")
+            return redirect('home')
+    
+    return render(request, "authentication/signin.html")
+
 
 def signout(request):
-    pass
+    logout(request)
+    messages.success(request, "Logged Out Successfully!!")
+    return redirect('home')
+
+def activate(request,uidb64,token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        myuser = User.objects.get(pk=uid)
+    except (TypeError,ValueError,OverflowError,User.DoesNotExist):
+        myuser = None
+
+    if myuser is not None and generate_token.check_token(myuser,token):
+        myuser.is_active = True
+        # user.profile.signup_confirmation = True
+        myuser.save()
+        login(request,myuser)
+        messages.success(request, "Your Account has been activated!!")
+        return redirect('signin')
+    else:
+        return render(request,'activation_failed.html')
