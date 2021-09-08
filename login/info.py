e44e2f51cfb27a636aebea7546ee3d048fc54765
nandinichhajed <nandinichhajed08@gmail.com>
2021-09-08 22:01:36 +0530

    Updating info file

--- login/info.py
+++ login/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'nandiniiichhajed@gmail.com'
-EMAIL_HOST_PASSWORD = 'Hnnn_Lilo'
+EMAIL_HOST_USER = 'Email Id'
+EMAIL_HOST_PASSWORD = 'password'
 EMAIL_PORT = 587
